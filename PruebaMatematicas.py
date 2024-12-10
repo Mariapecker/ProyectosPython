4ea4251a00b44086aa2a7074d09646a4eaea6528
Mariapecker <mariapecker@gmail.com>
2024-12-10 13:09:13 +0100

    Matemáticas
    Algunas pruebas de números, operaciones y conversión entre tipos en Python

---
+++ PruebaMatematicas.py
@@ -0,0 +1,9 @@
+#Vamos a hacer unas pruebas con números y operaciones
+A = 25.2
+B = 57
+print(A+B)
+print(A**A)
+print(B^(int(A)))#para poder elevar con '^' tienen que ser 2 enteros, por tanto usamos int
+C = B/A
+print(C)
+print((A+B)*C)
