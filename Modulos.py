ecc7c0ab5e516af398d1d24e807e1ab9036f886b
Mariapecker <mariapecker@gmail.com>
2024-12-12 15:54:13 +0100

    Ejercicio modulo

---
+++ Modulos.py
@@ -0,0 +1,7 @@
+from mensajes import mensaje
+
+#En este fichero vamos a emplear la función mensahe que hemos definido en el fichero mensajes importándola y comprobando que podemos usarla on un par de ejemplos
+
+print("Esto es una prueba para importar métodos de otros ficheros:")
+print(mensaje("ERROR 404 not found", "error"))
+print(mensaje("PELIGRO animales peligrosos", "warning"))
